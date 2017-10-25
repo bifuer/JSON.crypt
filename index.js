e6f614d463ba955803ee3e11966248223722e8ff
Javier Gallego Martín <bifuer@gmail.com>
2017-10-25 11:19:58 +0200

    New support for cipheriv

--- index.js
+++ index.js
@@ -10,31 +10,34 @@ if(global.modulesCache['json.crypt']){
 
 var crypto = require('crypto');
 
-JSON.cryptPassword = 'none';
-JSON.cryptAlgorithm = 'aes-256-ctr';
+JSON.cryptPassword = crypto.randomBytes(32);
+JSON.cryptAlgorithm = 'AES-256-CBC';
 
-JSON.encrypt = function(data,password,algorithm){
+JSON.encrypt = function(data, password, algorithm){
 	password = password || JSON.cryptPassword;
 	algorithm = algorithm || JSON.cryptAlgorithm;
+	var iv = new Buffer(crypto.randomBytes(16));
 	try {
 		data = JSON.decycled(data);
-		var cipher = crypto.createCipher(algorithm,password);
-		data = cipher.update(data,'utf8','hex');
+		var cipher = crypto.createCipheriv(algorithm, password, iv);
+		data = cipher.update(data, 'utf8', 'hex');
 		data += cipher.final('hex');
 	} catch(error){
 		JSON.cryptError = error;
 		return;
 	}
-	return data;
+	return iv.toString('hex') + ':' + data;
 };
 
-JSON.decrypt = function(data,password,algorithm){
-	data = data || '';
+JSON.decrypt = function(data, password, algorithm){
+	data = (data || '').split(':');
+	var iv = new Buffer(data[0], 'hex');
+	data = data[1];
 	password = password || JSON.cryptPassword;
 	algorithm = algorithm || JSON.cryptAlgorithm;
-	var decipher = crypto.createDecipher(algorithm,password);
+	var decipher = crypto.createDecipheriv(algorithm, password, iv);
 	try {
-		data = decipher.update(data,'hex','utf8');
+		data = decipher.update(data, 'hex', 'utf8');
 		data += decipher.final('utf8');
 		data = JSON.revive(data);
 	} catch(error){
